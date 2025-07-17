c118f25fe9401b7d5dace35b30af3c9eb8b9be96
Antonia Elsen <antoniaelsen@gmail.com>
2025-07-17 12:29:52 -0700

    chore: update nameplate styling

--- src/lib/client/components/constellations/Nameplate.svelte
+++ src/lib/client/components/constellations/Nameplate.svelte
@@ -12,12 +12,12 @@
 </script>
 
 <Card
-	class="flex w-max flex-row flex-nowrap items-center justify-start gap-2 overflow-hidden px-4 py-3"
+	class="flex w-max flex-row flex-nowrap items-center justify-start gap-3 overflow-hidden px-4 py-3"
 >
 	<Avatar src="https://placecats.com/300/200" cornerStyle="rounded" class="pointer-events-none" />
 
 	<div class="flex flex-col">
-		<A class="text-gray-900 dark:text-gray-100" size="md" {href}>
+		<A class="text-gray-900 hover:no-underline dark:text-gray-100" size="md" {href}>
 			{name}
 		</A>
 
@@ -27,7 +27,7 @@
 					class="flex-inherit flex flex-row flex-nowrap items-center justify-start gap-0 overflow-hidden text-gray-500 dark:text-gray-500"
 				>
 					<A
-						class="text-ellipsis whitespace-nowrap text-inherit hover:no-underline! dark:text-inherit"
+						class="text-ellipsis whitespace-nowrap text-inherit dark:text-inherit"
 						size="sm"
 						href={artist.href}
 					>
