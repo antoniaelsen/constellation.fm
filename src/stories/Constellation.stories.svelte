f5d670f65f66a7dcd628fcf488d4a8c80f65df3b
Antonia Elsen <antonia@blacksignal.fm>
2025-07-28 18:41:58 -0700

    test: fix Storybook threlte component tests, add ci workflow

--- src/stories/Constellation.stories.svelte
+++ src/stories/Constellation.stories.svelte
@@ -8,7 +8,6 @@
 	const { Story } = defineMeta({
 		title: 'constellations/Constellation',
 		component: Constellation,
-		decorators: [() => SceneDecorator as any],
 		tags: ['autodocs'],
 		parameters: {
 			layout: 'fullscreen'
@@ -19,35 +18,37 @@
 	const CONSTELLATION = CONSTELLATIONS[0];
 </script>
 
-<Story
-	name="Basic"
-	args={{
-		activeNodeId: null,
-		showNameplates: false,
-		constellation: CONSTELLATION,
-		selectedNodeIds: [],
-		onStarClick: fn(),
-		onStarButtonClick: fn(),
-		onEdgeRemove: fn()
-	}}
->
-	{#snippet template(args)}
-		<Constellation
-			activeNodeId={args.activeNodeId}
-			constellation={args.constellation}
-			showNameplates={args.showNameplates}
-			selectedNodeIds={args.selectedNodeIds}
-			onStarClick={(star, e) => {
-				if (!e.nativeEvent.shiftKey) {
-					return;
-				}
-				if (args.selectedNodeIds.includes(star.id)) {
-					args.selectedNodeIds = args.selectedNodeIds.filter((id) => id !== star.id);
-				} else {
-					args.selectedNodeIds.push(star.id);
-				}
-				args.onStarClick?.(star, e);
-			}}
-		/>
-	{/snippet}
-</Story>
+<SceneDecorator>
+	<Story
+		name="Basic"
+		args={{
+			activeNodeId: null,
+			showNameplates: false,
+			constellation: CONSTELLATION,
+			selectedNodeIds: [],
+			onStarClick: fn(),
+			onStarButtonClick: fn(),
+			onEdgeRemove: fn()
+		}}
+	>
+		{#snippet template(args)}
+			<Constellation
+				activeNodeId={args.activeNodeId}
+				constellation={args.constellation}
+				showNameplates={args.showNameplates}
+				selectedNodeIds={args.selectedNodeIds}
+				onStarClick={(star, e) => {
+					if (!e.nativeEvent.shiftKey) {
+						return;
+					}
+					if (args.selectedNodeIds.includes(star.id)) {
+						args.selectedNodeIds = args.selectedNodeIds.filter((id) => id !== star.id);
+					} else {
+						args.selectedNodeIds.push(star.id);
+					}
+					args.onStarClick?.(star, e);
+				}}
+			/>
+		{/snippet}
+	</Story>
+</SceneDecorator>
--- src/stories/Star.stories.svelte
+++ src/stories/Star.stories.svelte
@@ -9,7 +9,6 @@
 	const { Story } = defineMeta({
 		title: 'constellations/Star',
 		component: Star,
-		decorators: [() => SceneDecorator as any],
 		tags: ['autodocs'],
 		parameters: {
 			layout: 'fullscreen'
@@ -21,39 +20,41 @@
 	});
 </script>
 
-<Story
-	name="Basic"
-	args={{
-		activeColor: 'cyan',
-		color: 'white',
-		scale: 1,
-		showNameplate: true,
-		index: 0,
-		isActive: false,
-		isSelected: false,
-		metadata: mockTrack(),
-		onClick: fn(),
-		onNameplateButtonClick: fn()
-	}}
->
-	{#snippet template(args)}
-		<Star
-			activeColor={args.activeColor}
-			color={args.color}
-			scale={args.scale}
-			showNameplate={args.showNameplate}
-			index={args.index}
-			isActive={args.isActive}
-			isSelected={args.isSelected}
-			metadata={args.metadata}
-			onClick={(e: IntersectionEvent) => {
-				args.onClick?.(e);
-				if (!e.nativeEvent.altKey) {
-					return;
-				}
+<SceneDecorator>
+	<Story
+		name="Basic"
+		args={{
+			activeColor: 'cyan',
+			color: 'white',
+			scale: 1,
+			showNameplate: true,
+			index: 0,
+			isActive: false,
+			isSelected: false,
+			metadata: mockTrack(),
+			onClick: fn(),
+			onNameplateButtonClick: fn()
+		}}
+	>
+		{#snippet template(args)}
+			<Star
+				activeColor={args.activeColor}
+				color={args.color}
+				scale={args.scale}
+				showNameplate={args.showNameplate}
+				index={args.index}
+				isActive={args.isActive}
+				isSelected={args.isSelected}
+				metadata={args.metadata}
+				onClick={(e: IntersectionEvent) => {
+					args.onClick?.(e);
+					if (!e.nativeEvent.altKey) {
+						return;
+					}
 
-				args.isSelected = !args.isSelected;
-			}}
-		/>
-	{/snippet}
-</Story>
+					args.isSelected = !args.isSelected;
+				}}
+			/>
+		{/snippet}
+	</Story>
+</SceneDecorator>
